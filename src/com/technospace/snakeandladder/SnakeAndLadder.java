754b02e74e11fbf94fa85c67eb71bf6d4122c54d
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-16 22:43:59 +0530

    [MODIFIED]:[Prerana] Checking the option to play as NO_PLAY,LADDER or SNAKE

--- src/com/technospace/snakeandladder/SnakeAndLadder.java
+++ src/com/technospace/snakeandladder/SnakeAndLadder.java
@@ -9,6 +9,9 @@ public class SnakeAndLadder {
 		 * Variable for defining player position in game
 		 */
 			int position=0;
+			final int NO_PLAY = 1;
+			final int LADDER = 2;
+			final int SNAKE = 3;
 			/***
 			 * Random class object to generate Die Number between 1-6
 			 */
@@ -17,10 +20,29 @@ public class SnakeAndLadder {
 			System.out.println("Player 1 start Position is = "+position);
 			int dieNo = random.nextInt(6)+1;
 			System.out.println("Die Number="+dieNo);
-			position = position + dieNo;
+			
 			System.out.println("New Position after Rolling Die="+position);
-			
-			
+			int option =random.nextInt(3)+1;
+			System.out.println("Option Number="+option);
+			switch(option){
+			case NO_PLAY:
+				System.out.println("Option = NO PLAY");
+				position = position;
+				break;
+			case LADDER:
+				System.out.println("option = LADDER");
+				position = position + dieNo;
+				break;
+			case SNAKE:
+				System.out.println("Option = SNAKE");
+				position = position - dieNo;
+				if(position < 0){
+					position = 0;
+				}
+				
+				break;
+			}
+			System.out.println("New Position = "+position);
 	}
 
 }
