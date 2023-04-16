e2580814399744baae2da05aff0f497da59ce982
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-16 16:56:11 +0530

    [ADDED]:[Prerana]Snake and Ladder Project setup with Initial Position

---
+++ src/com/technospace/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.technospace.snakeandladder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/***
+		 * Variable for defining player position in game
+		 */
+			int position=0;
+			System.out.println("Player 1 Position = "+position);
+			
+	}
+
+}
